22e3a707c008563e9df71f808ceb47a3d84eef7e
manuqiao <manuqiao@me.com>
2013-08-25 22:04:58 +0800

    initial commit
    加入了body

--- TableTennis/MyScene.m
+++ TableTennis/MyScene.m
@@ -16,16 +16,31 @@
         
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        [self addEdge];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        [self addChild:myLabel];
+        sprite.position = CGPointMake(100, 200);
+        
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        
+        [sprite runAction:[SKAction repeatActionForever:action]];
+        [sprite setScale:0.1f];
+        SKPhysicsBody *body = [SKPhysicsBody bodyWithRectangleOfSize:sprite.frame.size];
+        [sprite setPhysicsBody:body];
+        
+        
+        [self addChild:sprite];
+        
+        sprite.physicsBody.affectedByGravity = YES;
+        self.physicsWorld.gravity = CGPointMake(0, -10);
     }
     return self;
+}
+
+- (void)addEdge
+{
+    
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -34,15 +49,7 @@
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
     }
 }
 
